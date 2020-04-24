fb6cd77a31bda64797c416893bf49722a11c9276
lmonty22 <lindsay.montgomery@olympian.org>
2020-04-23 20:15:43 -0400

    works except for when you add a new monster then hit next

--- js/index.js
+++ js/index.js
@@ -1,28 +1,51 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     renderForm()
-    fetchMonsters()
-    
-
+    initialFetch()
+    let backButton = document.querySelector('#back')
+    let forwardButton = document.querySelector('#forward')
+    forwardButton.onclick = (handleForward)
+    backButton.onclick = handleBack
 })
 
+function initialFetch() {
+        //fetch the first 50 Monsters and render them to DOM with name, age and description 
+        fetch('http://localhost:3000/monsters')
+        .then(resp => resp.json())
+        .then(array => renderMonsterList(array, 0, "forward"))
+ }
 
 // When the page loads, show the first 50 monsters. 
 //Each monster's name, age, and description should be shown.
-function fetchMonsters() {
-    //fetch the first 50 Monsters and render them to DOM with name, age and description 
-    fetch('http://localhost:3000/monsters')
-    .then(resp => resp.json())
-    .then(monstersArray => { 
-        for (let i = 0; i < 50; i++) {
-            renderMonster(monstersArray[i])
-          }
-    })
+
+
+function renderMonsterList(monsterArray, num, direction) {
+// needs to return an array of only 50 monsters at a time 
+let monsterDiv = document.querySelector('#monster-container')
+monsterDiv.innerHTML = ''
+let end = num + 50 
+if (direction === "forward"){
+    end = num + 50 
+    for (let i = num; i < end; i++) {
+        renderMonster(monsterArray[i])
+        }
+    }
+else 
+    {let start = num - 50 
+    if (start < 0)
+        {for (let i = 0; i < 50; i++) {
+        renderMonster(monsterArray[i])
+        }}
+    else { for (let i = start; i < num; i++) {
+        renderMonster(monsterArray[i])}}
+    }
+
 }
 
 function renderMonster(monster) {
     let monsterDiv = document.querySelector('#monster-container')
     let monsterCard = document.createElement("div")
+    monsterCard.id = monster.id
     monsterDiv.appendChild(monsterCard)
     let nameNode = document.createElement("h2")
     nameNode.innerText= monster.name
@@ -59,7 +82,37 @@ function renderForm() {
 
 function createMonster(e) {
     e.preventDefault()
-    
-    
+    formNode = e.currentTarget
+    ageField = document.querySelector('#age-input')
+    nameField = document.querySelector('#name-input')
+    descriptionField = document.querySelector('#description-input')
+    monsterObj = {
+        name: nameField.value,
+        age: ageField.value,
+        description: descriptionField.value
+    }
+        fetch('http://localhost:3000/monsters', {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(monsterObj)
+        })
+        .then (resp => resp.json())
+        .then(newMonster => renderMonster(newMonster))
+    formNode.reset()
 }
-// At the end of the list of monsters, show a button. When clicked, the button should load the next 50 monsters and show them.
+
+function handleForward(){
+    monsterDiv = document.querySelector('#monster-container')
+    let sv = Number(monsterDiv.lastChild.id)
+    fetch('http://localhost:3000/monsters')
+    .then(resp => resp.json())
+    .then(array => renderMonsterList(array, sv, "forward"))
+}
+
+function handleBack() {
+    monsterDiv = document.querySelector('#monster-container')
+    let sv = Number(monsterDiv.lastChild.id) - 50 
+    fetch('http://localhost:3000/monsters')
+    .then(resp => resp.json())
+    .then(array => renderMonsterList(array, sv, "back"))
+}
